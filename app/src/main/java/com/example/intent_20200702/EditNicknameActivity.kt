af0fd87440702a7d1d3eb57aeefe59e9f2725a44
박호준 <hankil417@naver.com>
2020-07-02 21:09:25 +0900

    Intent의 세번쨰 사용법 연습 화면 그리기 닉네임 변경 가능 화면 구성

---
+++ app/src/main/java/com/example/intent_20200702/EditNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_20200702
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+}
